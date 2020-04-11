276152a05148af5fca5fd7ece38b0f8c27d242ac
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2020-04-11 21:29:55 +0700

    remove log unused

--- auth/auth.go
+++ auth/auth.go
@@ -3,7 +3,6 @@ package auth
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"cloud.google.com/go/firestore"
@@ -53,39 +52,29 @@ func Login(fs *firestore.Client) http.HandlerFunc {
 		}
 
 		ctx := context.Background()
-		fmt.Println("##### Login: ctx := context.Background()")
 		iter := fs.Collection("userData").Where("email", "==", credential.Email).
 			Where("password", "==", credential.Password).
 			Documents(ctx)
-		fmt.Println("##### Login: fs.Collection")
 
 		defer iter.Stop()
 
 		for {
-			fmt.Println("##### Login: for")
 			doc, err := iter.Next()
-			fmt.Println("##### Login: Next")
 			if err == iterator.Done {
-				fmt.Println("##### Login: Done")
 				break
 			}
+			
 			if err != nil {
-				fmt.Println("##### Login: for nil")
-				response.InternalServerError(w, err)
 				break
 			}
 
 			p := LoginResponse{}
+			p.ID = doc.Ref.ID
 			err = doc.DataTo(&p)
-			fmt.Println("##### Login: for doc.DataTo(&p)")
 			if err != nil {
-				response.InternalServerError(w, err)
 				break
 			}
 
-			p.ID = doc.Ref.ID
-
-			fmt.Println("##### Login: json.NewEncoder(w).Encode(&p)")
 			json.NewEncoder(w).Encode(&p)
 			return
 		}
