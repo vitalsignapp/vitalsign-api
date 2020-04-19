7697995191f9b5cb6c563105534ebdd5857b3dff
guutong <pornmongkonp@gmail.com>
2020-04-20 00:09:45 +0700

    add test logout

---
+++ auth/auth_test.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestLogout(t *testing.T) {
+	t.Run("it should return httpCode 200 and set cookie accessToken is empty when call /logout", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "/logout", nil)
+		if err != nil {
+			t.Error(err)
+		}
+		resp := httptest.NewRecorder()
+		handler := http.HandlerFunc(Logout())
+		handler.ServeHTTP(resp, req)
+
+		if status := resp.Code; status != http.StatusOK {
+			t.Errorf("wrong code: got %v want %v", status, http.StatusOK)
+		}
+
+		if cookie := resp.Header().Get("Set-Cookie"); cookie != "accessToken=; Path=/" {
+			t.Errorf("wrong code: got %v want %v", cookie, "accessToken=; Path=/")
+		}
+	})
+}
